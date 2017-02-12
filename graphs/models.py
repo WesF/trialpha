24ecb1010d78c0036f47846a586aace95cf89d76
WesF <weslfield@gmail.com>
2017-02-12 21:33:09 +0000

    added C2 model and dummy data

--- graphs/models.py
+++ graphs/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+@python_2_unicode_compatible
+class C2(models.Model):
+    radius = models.FloatField(default=0.0)
+    milliseconds = models.FloatField(default=0.0)
+    experiment_id = models.IntegerField()
+    
+    def __str__(self):
+        return str(self.experiment_id) + '_' + str(self.milliseconds)
