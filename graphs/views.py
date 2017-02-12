395a058bc8a5aff2ef977eb1bbe7a8024315aa07
WesF <weslfield@gmail.com>
2017-02-12 21:53:29 +0000

    set up index

--- graphs/views.py
+++ graphs/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import C2
 
 def index(request):
+    #TODO: process C2 data into some better data structure depending on
+    #      what the 3 libraries want :-)
+    data = C2.objects.all()
+    template = loader.get_template('polls/index.html')
+    context = {
+        'data': data,
+    }
     return HttpResponse("Hello, world.")
