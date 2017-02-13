1fc4eed1dc5613f586eb25e4120f58c87d5d1698
WesF <weslfield@gmail.com>
2017-02-13 01:06:36 +0000

    set up static files

--- trialpha/settings.py
+++ trialpha/settings.py
@@ -120,5 +120,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "trialpha/trialpha/static"),
+    os.path.join(BASE_DIR, "trialpha/graphs/static")
+]
+
+print "static files dir paths", STATICFILES_DIRS
