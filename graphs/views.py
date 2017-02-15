528c5783cc4e046c943395b4cef9f8948d5d9f87
WesF <weslfield@gmail.com>
2017-02-15 23:31:56 +0000

    changes to routing and d3

--- graphs/views.py
+++ graphs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
 from django.core import serializers
+from django.views.generic import ListView
 
 from .models import C2
 
@@ -16,7 +17,12 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
+
 def data(request):
     data = C2.objects.all()
     json_data = [ob.as_json() for ob in data]
     return json_data
+
+class C2ListView(ListView):
+    model = C2
+    
--- trialpha/urls.py
+++ trialpha/urls.py
@@ -18,6 +18,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^graphs/', include('graphs.urls')),
+    url(r'^', include("graphs.urls", namespace="graphs", app_name="graphs")),
     url(r'^admin/', admin.site.urls),
 ]
